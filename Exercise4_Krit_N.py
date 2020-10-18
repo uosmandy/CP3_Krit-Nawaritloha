629e5a0b2829ea6e774dd4de4c49ea2ee5b82805
uosmandy <uosmandy@github.com>
2020-10-18 13:16:51 +0700

    Create Exercise4_Krit_N.py
    Krit Nawaritloha ได้ส่งการบ้าน Exercise 4 เรียบร้อยแล้ว

---
+++ Exercise4_Krit_N.py
@@ -0,0 +1,13 @@
+FE = 60.5
+GB = 80.4
+IC = 25.0
+CP = 60.2
+
+print("---------------")
+print("Score System")
+print("---------------")
+print("--Your Score--")
+print("Foudation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", IC)
+print("Computer Programming :", CP)
