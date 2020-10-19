2f75abd77e256a80cdd1f7d0efe72a08fc08f32a
uosmandy <73017964+uosmandy@users.noreply.github.com>
2020-10-19 11:07:44 +0700

    Exercise5

---
+++ Exercise5_1.py
@@ -0,0 +1,6 @@
+x = 10
+y = 2
+print(str(x), "+", str(y), "=", x+y)
+print(str(x), "-", str(y), "=", x-y)
+print(str(x), "*", str(y), "=", x*y)
+print(str(x), "/", str(y), "=", int(x/y))
---
+++ Exercise5_2.py
@@ -0,0 +1,8 @@
+print("**********")
+print("Motion Distancing")
+print("**********")
+
+s = int(input("spatial :"))
+t = int(input("times :"))
+v = s/t
+print("Velocity =", v,"km/h")
